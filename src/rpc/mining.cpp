d55e2cfd68c84271e7d61a8e1935848c3143294a
Laksh Gandikota <laksh@gandikota.com>
2020-02-03 23:55:11 -0800

    Branding updates

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -465,9 +465,9 @@ UniValue getblocktemplate(const JSONRPCRequest& request)
     if(!g_connman)
         throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
 
-    if (g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0)
-        throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "TEKoin is not connected!");
-
+    // if (g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0)
+    //     throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "TEKoin is not connected!");
+    //
     if (IsInitialBlockDownload())
         throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "TEKoin is downloading blocks...");
 
